e433ada9b2902a110792db0bcee2a880b3a2f6dc
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2020-06-21 23:19:26 -0700

    CSS for chroma syntax highlight

--- serve.sh
+++ serve.sh
@@ -12,6 +12,10 @@ echo "\033[0;1mBuilding for environment: \033[0;1;35mdevelopment\033[0;0m"
 echo "\033[0;1mRefreshing theme dependencies\033[0;0m"
 (cd themes/withblueink && npm install)
 
+# Syntax highlighting CSS
+echo "\033[0;1mSyntax highlighting CSS \033[0;0m"
+hugo gen chromastyles --style=monokailight > themes/withblueink/assets/css/chroma-monokailight.css
+
 # Compile the code with the "production" environment
 echo "\033[0;1mBuilding and starting web server...\033[0;0m"
 hugo serve --environment=development --buildFuture --buildDrafts --disableFastRender
