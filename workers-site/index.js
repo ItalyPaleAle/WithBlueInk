6482ba0c818ff1d1b2002b43e2d1f21aae299f9e
Alessandro Segala (ItalyPaleAle) <43508+ItalyPaleAle@users.noreply.github.com>
2021-07-15 20:10:03 -0700

    Turned off debug mode

--- workers-site/index.js
+++ workers-site/index.js
@@ -10,7 +10,7 @@ import contentSecurityPolicy from './content-security-policy'
  * 1. we will skip caching on the edge, which makes it easier to debug
  * 2. we will return an error message on exception in your Response rather than the default 404.html page
  */
-const DEBUG = true
+const DEBUG = false
 
 addEventListener('fetch', (event) => {
     try {
