c4572a816033b3cba92d15dd0aea8d71ff1c2423
Alessandro Segala (ItalyPaleAle) <43508+ItalyPaleAle@users.noreply.github.com>
2021-09-07 18:27:37 -0700

    Added /go/git-sign short link

--- workers-site/redirects.js
+++ workers-site/redirects.js
@@ -2,6 +2,7 @@ export default {
     // Short links
     '/go/docker-rpi': '/2020/06/24/docker-and-docker-compose-on-raspberry-pi-os.html',
     '/go/auth': '/2020/04/08/stop-writing-your-own-user-authentication-code.html',
+    '/go/git-sign': '/2020/05/17/how-and-why-to-sign-git-commits.html',
 
     // Old URLs
     '/2017/12/31/yes-you-can-run-docker-on-raspbian.html': '/2019/07/13/yes-you-can-run-docker-on-raspbian.html',
