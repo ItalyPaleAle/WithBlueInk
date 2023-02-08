6f915959781e772d158c096fcc97e6d44523af76
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2023-02-07 20:45:50 -0800

    Updated deps

--- dist.sh
+++ dist.sh
@@ -22,10 +22,6 @@ echo "\033[0;1mBuilding...\033[0;0m"
 #hugo --environment=production --buildFuture
 hugo --environment=production --minify
 
-# Copy static files
-#echo "\033[0;1mCopy static files...\033[0;0m"
-#cp -v _statiko.yaml public/
-
 # Remove files that shouldn't be published
 echo "\033[0;1mRemoving unnecessary files...\033[0;0m"
 rm -v public/*.sh || true
