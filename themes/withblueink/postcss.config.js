1bb6d8c41b483d0889ab0f59d370cf5b2b669605
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2026-05-09 12:45:09 -0700

    Fix build

--- themes/withblueink/postcss.config.js
+++ themes/withblueink/postcss.config.js
@@ -1,3 +1,5 @@
+const path = require('node:path')
+
 const purgecss = require('@fullhuman/postcss-purgecss')({
     content: ['./hugo_stats.json'],
     preserveHtmlElements: false,
@@ -7,15 +9,43 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
     }
 })
 
+const browserslist = [
+    'last 1 version',
+    '> 1%',
+    'maintained node versions',
+    'not dead'
+]
+
+const themeCssPath = path.join(__dirname, 'assets/css')
+const localCssImports = new Set([
+    'font-cascadia-code.css',
+    'font-lato.css',
+    'font-merriweather.css',
+    'chroma.css',
+    'themes.css'
+])
+
 module.exports = {
     plugins: [
         require('postcss-import')({
-            path: __dirname + '/assets/css'
+            path: [themeCssPath],
+            resolve: (id) => {
+                const normalizedId = id.replace(/^\.\//, '')
+
+                if (localCssImports.has(normalizedId)) {
+                    return path.join(themeCssPath, normalizedId)
+                }
+
+                return id
+            }
         }), 
         require('tailwindcss')({
             config: __dirname + '/tailwind.config.js'
         }),
-        require('autoprefixer'),
+        require('autoprefixer')({
+            overrideBrowserslist: browserslist,
+            stats: {}
+        }),
         ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
     ]
 }
